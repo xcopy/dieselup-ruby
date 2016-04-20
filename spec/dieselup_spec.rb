9bfb974ace370bc718477d86c30a03a35a84feab
Kairat Jenishev <kairat.jenishev@gmail.com>
2016-04-20 22:53:27 +0600

    More tests, try http request

--- spec/dieselup_spec.rb
+++ spec/dieselup_spec.rb
@@ -4,4 +4,25 @@ describe Dieselup do
   it 'has a version number' do
     expect(Dieselup::VERSION).not_to be nil
   end
+
+  it 'has correct URLs' do
+    expect(Dieselup::Url::BASE).to eq 'https://diesel.elcat.kg/index.php'
+    expect(Dieselup::Url.get({showtopic: 1})).to eq 'https://diesel.elcat.kg/index.php?showtopic=1'
+  end
+
+  it 'should send request' do
+    url = URI.parse(Dieselup::Url::BASE)
+    http = Net::HTTP.new(url.host, url.port)
+
+    if url.scheme == 'https'
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    request = Net::HTTP::Get.new(url.to_s)
+    response = http.request(request)
+
+    expect(response.code).to eq '200'
+    expect(response.content_type).to eq 'text/html'
+  end
 end
